a7ed075f26c5fcc8da23e581c19971a6a4555a66
kortkamp <marcelusmedius@gmail.com>
2022-12-04 15:00:06 -0300

    fix: add new standard sizes used in tailwind configs

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -19,7 +19,7 @@ export default function Footer() {
             <div className="container mx-auto w-9/12">
                 <div className="flex flex-col">
                     <div className="flex flex-row justify-between mt-5 mb-9 flex-wrap">
-                        <h3 className={`${inter.className} text-2xl text-white`}>Trayllo</h3>
+                        <h3 className={`${inter.className} text-lg text-white`}>Trayllo</h3>
                         <div className={`${openSans.className} flex items-end text-base font-light`}>
                             <Link href="#" className={`${openSans.className} mr-10 text-white`}>Política de Privacidade</Link>
                             <Link href="#" className={`${openSans.className} text-white`}>Termos de Uso</Link>
@@ -27,12 +27,12 @@ export default function Footer() {
                     </div>
                     <hr className="opacity-20"/>
                     <div className="flex flex-row justify-between mt-9 mb-4">
-                        <p className="font-light text-white">@2022 Trayllo</p>
+                        <p className="font-light text-white text-xs">@2022 Trayllo</p>
                         <div className="flex flex-row justify-between">
-                            <Link href="#"><FontAwesomeIcon icon={faInstagram} className="mx-4 text-xl text-white"/></Link>
-                            <Link href="#"><FontAwesomeIcon icon={faLinkedin} className="mx-4 text-xl text-white"/></Link>
-                            <Link href="#"><FontAwesomeIcon icon={faFacebook} className="mx-4 text-xl text-white"/></Link>
-                            <Link href="#"><FontAwesomeIcon icon={faTwitter} className="mx-4 text-xl text-white"/></Link>
+                            <Link href="#"><FontAwesomeIcon icon={faInstagram} className="mx-4 text-md text-white"/></Link>
+                            <Link href="#"><FontAwesomeIcon icon={faLinkedin} className="mx-4 text-md text-white"/></Link>
+                            <Link href="#"><FontAwesomeIcon icon={faFacebook} className="mx-4 text-md text-white"/></Link>
+                            <Link href="#"><FontAwesomeIcon icon={faTwitter} className="mx-4 text-md text-white"/></Link>
                         </div>
                     </div>
                 </div>
