3ea4c011da6a15cc7ab9e59b45d8004a3164392e
Lucas-E <lucasgitacc@gmail.com>
2022-12-05 02:06:56 -0300

    added images, altered global.css, imported image from next

--- app/components/CardPersonalizado/CardPersonalizado.jsx
+++ app/components/CardPersonalizado/CardPersonalizado.jsx
@@ -1,6 +1,9 @@
 import { Fragment } from "react"
 import { Open_Sans } from "@next/font/google"
 
+import Image8 from '../../public/images/image8.png'
+import Image9 from '../../public/images/image9.png'
+
 const openSans = Open_Sans({
 
 })
@@ -9,8 +12,8 @@ import Image from "next/image"
 export default function CardPersonalizado() {
   return (
     <Fragment>
-        <section className="bg-section flex flex-col justify-center p-10 transition-transform">
-            <div className="container mx-auto w-9/12 flex flex-row justify-center border-2 h-96">
+        <section className="bg-section flex flex-col justify-center p-5 h-content h-screen">
+            <div className="container mx-auto w-10/12 flex flex-row justify-center">
                 <div className="flex flex-col w-100 md:w-1/2 justify-center">
                     <p className="text-[3em] text-[#490177] font-extrabold mb-5 leading-snug">Personalize seus cards no Trello</p>
                     <p class="text-slate-600 mb-5">
@@ -19,15 +22,9 @@ export default function CardPersonalizado() {
                     <button className="align-center p-5 text-center bg-[#490177] rounded hover:bg-[#320152] w-52 font-bold text-xl">Comece Agora</button>
                 </div>
                 <div className="md:flex flex-col items-center w-1/2 hidden">
-                    <div className="relative">
-                        <div className="absolute">
-                            <div className="border-2 h-100">teste</div>
-                        </div>
-                    </div>
-                    <div className="relative">
-                        <div className="absolute">
-                            <div className="border-2 h-100 translate-y-2">teste2</div>
-                        </div>
+                    <div className="parent translate-y-10 md:-translate-y-10 lg:-translate-y-20">
+                        <Image src={Image8} className="overlay translate-x-1/3 img-responsive"/>
+                        <Image src={Image9} className="overlay translate-y-40 img-responsive"/>
                     </div>
                 </div>
             </div>
