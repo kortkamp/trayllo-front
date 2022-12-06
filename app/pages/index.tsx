02d4366b0b6d85f03dbb818713ed1f926a54b14f
kortkamp <marcelusmedius@gmail.com>
2022-12-06 12:55:38 -0300

    feat: get page data from API

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,14 +1,20 @@
 import CtaSection from '../components/CtaSection'
-import HeroSection from '../components/HeroSection'
+import HeroSection, {HeroPropsData} from '../components/HeroSection'
 import Footer from '../components/Footer/Footer'
 import { Fragment } from 'react'
 import ExtrasSection from '../components/ExtrasSection'
 import StepsSection from '../components/StepsSection'
 
-export default function index() {
+interface PageProps {
+  pageData: {
+    sectionHeroData: HeroPropsData 
+  }
+}
+
+export default function index({pageData}: PageProps) {
   return (
     <div className=''>
-      <HeroSection />
+      <HeroSection data={pageData.sectionHeroData} />
       <CtaSection />
       <StepsSection />
       <ExtrasSection />
@@ -17,4 +23,17 @@ export default function index() {
       </Fragment>
     </div>
   )
+}
+
+
+export async function getStaticProps():Promise<{ props:PageProps }> {
+  const res = await fetch('https://trayllo.com.br/wp-json/trayllo/public/section-hero')
+  const sectionHeroData = await res.json()
+  return {
+    props: {
+      pageData: {
+        sectionHeroData
+      }
+    },
+  }
 }
