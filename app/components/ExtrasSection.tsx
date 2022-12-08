f300329323cbb2340c12c10f65ab8ce3ff6e6945
kortkamp <marcelusmedius@gmail.com>
2022-12-07 22:40:47 -0300

    feat: use new react-player component

--- app/components/ExtrasSection.tsx
+++ app/components/ExtrasSection.tsx
@@ -1,10 +1,9 @@
 import SafeArea from "./SafeArea";
 import YoutubeEmbed from "./YoutubeEmbed";
 
-const YT_VIDEO_ID = 'xky48zyL9iA'
 const VIDEO_WIDTH = 895
 const VIDEO_HEIGHT = 559
-//https://www.youtube.com/watch?v=xky48zyL9iA
+const videoUrl= 'https://www.youtube.com/watch?v=xky48zyL9iA'
 
 export default function ExtrasSection(){
 
@@ -14,7 +13,7 @@ export default function ExtrasSection(){
         <div className="">
           <header className="w-[558px] max-md:w-full m-auto"><h2 id='extras' className="text-primary text-xl max-md:text-lg font-extrabold text-center py-10 font-sans">O que posso fazer com essa integração?</h2></header>
           <div className="rounded-md overflow-hidden">
-            <YoutubeEmbed  embedId={YT_VIDEO_ID} width={VIDEO_WIDTH} height={VIDEO_HEIGHT}/>
+            <YoutubeEmbed videoUrl={videoUrl} width={VIDEO_WIDTH} height={VIDEO_HEIGHT}/>
           </div>
         </div>
       </SafeArea>
