774a71e57b3ca1c729f46348a401ec0ecc8f1265
kortkamp <marcelusmedius@gmail.com>
2022-12-07 22:40:27 -0300

    feat: get data from api for steps section

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -4,13 +4,15 @@ import Footer from '../components/Footer/Footer'
 import { Fragment } from 'react'
 import CardPersonalizado from '../components/CardPersonalizado/CardPersonalizado'
 import ExtrasSection from '../components/ExtrasSection'
-import StepsSection from '../components/StepsSection'
+import StepsSection, { StepsPropsData } from '../components/StepsSection'
 import SectionSobre from '../components/SectionSobre/Sobre'
+import { youtube_parser } from '../utils/youtube'
 
 interface PageProps {
   pageData: {
     sectionHeroData: HeroPropsData 
     sectionCtaData: CtaPropsData
+    sectionStepsData: StepsPropsData
   }
 }
 
@@ -19,7 +21,7 @@ export default function index({pageData}: PageProps) {
     <div className=''>
       <HeroSection data={pageData.sectionHeroData} />
       <CtaSection  data={pageData.sectionCtaData}/>
-      <StepsSection />
+      <StepsSection data={pageData.sectionStepsData}/>
       <CardPersonalizado></CardPersonalizado>
       <ExtrasSection />
       <SectionSobre />
@@ -31,18 +33,36 @@ export default function index({pageData}: PageProps) {
 }
 
 
+async function getVideoData(url:string){
+  const noembedRes = await fetch(`https://noembed.com/embed?url=${url}`)
+  const videoData = await noembedRes.json()
+
+  return {
+    id: youtube_parser(url) || '',
+    width: videoData.width,
+    height: videoData.height,
+  }
+
+}
+
 export async function getStaticProps():Promise<{ props:PageProps }> {
   const sectionHeroRes = await fetch('https://trayllo.com.br/wp-json/trayllo/public/section-hero')
   const sectionHeroData = await sectionHeroRes.json()
 
   const sectionCtaRes = await fetch('https://trayllo.com.br/wp-json/trayllo/public/section-cta')
   const sectionCtaData = await sectionCtaRes.json()
-  
+
+  const sectionStepsRes = await fetch('https://trayllo.com.br/wp-json/trayllo/public/section-passo-a-passo')
+  const sectionStepsData = await sectionStepsRes.json() as StepsPropsData
+
+  sectionStepsData.videoData = await getVideoData(sectionStepsData.video)
+
   return {
     props: {
       pageData: {
         sectionHeroData,
-        sectionCtaData
+        sectionCtaData,
+        sectionStepsData
       }
     },
   }
